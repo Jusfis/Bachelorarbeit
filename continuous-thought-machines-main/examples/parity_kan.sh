022c92e59d09b1bbf5b7bdbbeba2d264c76c04a8
justusfischer <justus.fischer@t-online.de>
2025-12-04 22:26:57 +0100

    uncomment sbatch directives for job submission in parity_kan.sh

--- continuous-thought-machines-main/examples/parity_kan.sh
+++ continuous-thought-machines-main/examples/parity_kan.sh
@@ -4,9 +4,10 @@
 #SBATCH --comment="CTM training"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=justus.fischer@campus.lmu.de
-#SBATCH --chdir=/home/fischerjus/Bachelorarbeit/continuous-thought-machines-main/examples
-#SBATCH --output=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/examples/slurm_kan.%j.%N.out
 #SBATCH --ntasks=1
+#SBATCH --chdir=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/examples
+#SBATCH --output=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/examples/slurm_kan.%j.%N.out
+
 
 python3 -u 04_parity_kan.py
 #to submit the job, use:
