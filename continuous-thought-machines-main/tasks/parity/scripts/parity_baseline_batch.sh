37a0d4277459e9c00c7ecb33b9d56b81090a2d28
justusfischer <justus.fischer@t-online.de>
2026-02-18 04:47:02 +0100

    Update batch script to set correct working directory for parity tasks

--- continuous-thought-machines-main/tasks/parity/scripts/parity_baseline_batch.sh
+++ continuous-thought-machines-main/tasks/parity/scripts/parity_baseline_batch.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=justus.fischer@campus.lmu.de
 #SBATCH --ntasks=1
-#SBATCH --chdir=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/tasks/parity
+#SBATCH --chdir=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/
 #SBATCH --output=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/tasks/parity/slurm_baseline.%j.%N.out
 
 #RUN=1
