b551a5b1d57a723c02a2745cad9a0c81cfd3c821
justusfischer <justus.fischer@t-online.de>
2026-03-01 23:13:12 +0100

    Update train_qamnist.py: adjust tracking interval and modify seed values for enhanced training dynamics

--- continuous-thought-machines-main/tasks/qamnist/train_qamnist.py
+++ continuous-thought-machines-main/tasks/qamnist/train_qamnist.py
@@ -311,6 +311,7 @@ def qamnist_model(args, config, run):
 
                         if bi % (args.track_every * 10) == 0  & bi != 0:
                             pbar.set_description('Tracking: Neural dynamics')
+                            print("Plooting neural dynamics")
                             plot_neural_dynamics(post_activations, 100, args.log_dir, axis_snap=True)
 
                         pbar.set_description('Tracking: Producing attention gif')
