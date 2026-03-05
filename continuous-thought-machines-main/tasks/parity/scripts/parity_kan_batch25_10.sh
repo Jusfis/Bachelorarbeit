5d704ab02b685cb4fa8a053a5f8492b9936cbaee
justusfischer <justus.fischer@t-online.de>
2026-03-05 15:02:57 +0100

    Add SLURM job script for KAN parity training: configure parameters and logging

---
+++ continuous-thought-machines-main/tasks/parity/scripts/parity_kan_batch25_10.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+#SBATCH --job-name=KAN_Parity
+#SBATCH --comment="CTM tuning with wandb"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=justus.fischer@campus.lmu.de
+#SBATCH --ntasks=1
+#SBATCH --chdir=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/
+#SBATCH --output=/home/f/fischerjus/Bachelorarbeit/continuous-thought-machines-main/tasks/parity/kan_final_100_50.%j.%N.out
+
+#RUN=1
+#ITERATIONS=10
+#MEMORY_LENGTH=5
+#LOG_DIR="logs/parity/run${RUN}/ctm_${ITERATIONS}_${MEMORY_LENGTH}"
+#SEED=$((RUN - 1))
+#    --model_type "ctm"\
+
+# IMPORTANT D_model % 5 == 0 for MLP postactivation production
+
+RUN=1
+LOG_DIR="logs/parity/kan"
+SEED=$((RUN - 1))
+MODEL="ctm"
+POSTACTIVATION="kan"
+ITERATIONS=25
+MEMORY_LENGTH=10
+
+export PYTHONPATH=$PYTHONPATH:.
+
+python -u tasks/parity/train_sweeps_efficient.py \
+    --log_dir $LOG_DIR \
+    --model_type $MODEL \
+    --seed 1 \
+    --iterations $ITERATIONS \
+    --memory_length $MEMORY_LENGTH \
+    --parity_sequence_length 64  \
+    --n_test_batches 20 \
+    --d_model 1024 \
+    --d_input 512 \
+    --n_synch_out 32 \
+    --n_synch_action 32 \
+    --synapse_depth 1 \
+    --heads 8 \
+    --memory_hidden_dims 16 \
+    --dropout 0.0 \
+    --deep_memory \
+    --no-do_normalisation \
+    --positional_embedding_type="custom-rotational-1d" \
+    --backbone_type="parity_backbone" \
+    --no-full_eval \
+    --weight_decay 0.0 \
+    --gradient_clipping 0.9 \
+    --use_scheduler \
+    --scheduler_type "cosine" \
+    --milestones 0 0 0 \
+    --gamma 0 \
+    --dataset "parity" \
+    --batch_size 64 \
+    --batch_size_test 256 \
+    --lr=0.0001 \
+    --training_iterations 300001 \
+    --warmup_steps 500 \
+    --track_every 10000 \
+    --save_every 50000 \
+    --no-reload \
+    --no-reload_model_only \
+    --no-use_amp \
+    --neuron_select_type "random" \
+    --device 0 \
+    --postactivation_production $POSTACTIVATION \
+    --useWandb 1
+
+
+# set --device 0 to allow slurm to assign GPU automatically
+# use Wandb set to 0 for local testing, set to 1 for slurm runs
+# to submit the job on slurm, use from ctm main folder:
+#sbatch --partition=NvidiaAll parity_kan_batch.sh script for slurm
